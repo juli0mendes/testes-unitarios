b53ccf01a842d84ec42f1d51c07b0cde567b83c7
Júlio Cesar Mendes <julio.mendes@sensedia.com>
2019-07-23 18:22:03 -0300

    Spy

--- src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
@@ -1,13 +1,51 @@
 package br.ce.wcaquino.servicos;
 
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 public class CalculadoraMockTest {
+	
+	@Mock
+	private Calculadora calcMock;
+	
+	@Spy
+	private Calculadora calcSpy;
+	
+	@Mock
+	private EmailService emailService;
+	
+	@Before
+	public void before() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void deveMostrarADiferencaEntreMockESpy() {
+		
+		when(calcMock.somar(1, 2)).thenReturn(5);
+//		when(calcSpy.somar(1, 2)).thenReturn(5);
+		doReturn(5).when(calcSpy).somar(1, 2);
+		
+		doNothing().when(calcSpy).imprime();
+		
+		System.out.println("Mock: " + calcMock.somar(1, 2));
+		System.out.println("Spy: " + calcSpy.somar(1, 2));
+		
+		System.out.println("Mock");
+		calcMock.imprime();
+		System.out.println("Spy");
+		calcSpy.imprime();
+	}
 
 	@Test
 	public void teste() {
